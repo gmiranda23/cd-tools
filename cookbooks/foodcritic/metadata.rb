8d735128cc07e4c5101aa211041008b8cff74b3d
gmiranda23 <gmiranda@opscode.com>
2013-09-26 08:10:09 -0700

    foodcritic handled by kitchen-jenkins

--- cookbooks/foodcritic/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Opscode Inc."
-maintainer_email "gmiranda@opscode.com"
-license          "All rights reserved"
-description      "Installs/Configures foodcritic"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.8"
-
-depends "build-essential"
